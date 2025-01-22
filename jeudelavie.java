2f189b87b18611aac038454b9a59f08d6a76a9c9
reliodas93 <reyanbabou9@gmail.com>
2025-01-22 23:21:33 +0100

    initialisation de la grille et des celulle vivante

--- jeudelavie.java
+++ jeudelavie.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
 
 public class jeudelavie {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        // Initialisation de la grille
+        System.out.print("Entrez le nombre de lignes : ");
+        int lignes = sc.nextInt();
+        System.out.print("Entrez le nombre de colonnes : ");
+        int colonnes = sc.nextInt();
+
+        boolean[][] grille = new boolean[lignes][colonnes];
+        boolean[][] nouvelleGrille = new boolean[lignes][colonnes];
+
+        // Initialisation des cellules vivantes
+        System.out.println("Entrez les coordonnées des cellules vivantes (ligne colonne). Entrez -1 -1 pour terminer :");
+        while (true) {
+            System.out.print("Ligne : ");
+            int ligne = sc.nextInt();
+            System.out.print("Colonne : ");
+            int colonne = sc.nextInt();
+
+            if (ligne == -1 && colonne == -1) break; // Terminer l'initialisation
+
+            if (ligne >= 0 && ligne < lignes && colonne >= 0 && colonne < colonnes) {
+                grille[ligne][colonne] = true;
+            } else {
+                System.out.println("Coordonnées invalides, réessayez !");
+            }
+        }
     }
 }
